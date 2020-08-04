a46bb4c371b67bf60fe2274dc1e8e87ae050072a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-04 19:27:18 -0300

    Criação do GET by Id e do Delete

--- routes/accounts.js
+++ routes/accounts.js
@@ -8,10 +8,10 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     try {
         let account = req.body;
-        const data = JSON.parse(await readFile(global.fileName ));
-        account = {id: data.nextId++, ...account};
+        const data = JSON.parse(await readFile(global.fileName));
+        account = { id: data.nextId++, ...account };
         data.accounts.push(account);
-        await writeFile(global.fileName , JSON.stringify(data, null, 2));
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
         res.send(account);
     } catch (err) {
         res.status(400).send({ erro: err.message })
@@ -19,13 +19,34 @@ router.post("/", async (req, res) => {
 });
 
 
-router.get("/", async (req, res)=>{
-    try{
-        let account = req.body;
-        const data = JSON.parse(await readFile(global.fileName ));
-    }catch(err){
+router.get("/", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data)
+    } catch (err) {
         res.status(400).send({ erro: err.message })
     }
 })
 
+router.get("/:id", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        const byId = data.accounts.find(account => parseInt(req.params.id) === account.id);
+        res.send(byId)
+    } catch (err) {
+        res.status(400).send({ erro: err.message })
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        data.accounts = data.accounts.filter(account => parseInt(req.params.id) !== account.id);
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.end();
+    } catch (err) {
+        res.status(400).send({ erro: err.message })
+    }
+});
 export default router;
